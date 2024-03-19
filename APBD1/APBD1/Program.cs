213a8c867ece7b4e62a82389ef50069f37f0e6a0
s28672 <s28672@pjwstk.edu.pl>
2024-03-19 02:58:07 +0100

    feature-average

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -1,3 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
 
 
+static double CalculateAverage(int[] arr) {
+    int sum = 0;
+    foreach(var a in arr)
+    {
+        sum += a;
+    }
+    return sum / arr.Length;
+}
